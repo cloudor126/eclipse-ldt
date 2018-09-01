1b904770a2b31c171ea2d0cfd29575137086844e
cloudor <cloudor@DESKTOP-DJ6RJR1>
2018-09-01 22:18:28 +0800

    清理无用文件

--- tests/org.eclipse.ldt.lua.tests/tests/formatter/reference/4spaces/basic/longstring.lua
+++
@@ -1,7 +0,0 @@
----
--- Long string to be process
---
-local string = [[Long string
-    4 space at the start of this line
-4 space at the end of this line    
-]]
--- tests/org.eclipse.ldt.lua.tests/tests/formatter/reference/mixed/basic/longstring2.lua
+++
@@ -1,7 +0,0 @@
----
--- Long string to be process
---
-local longString = [=[Long string
-    4 space at the start of this line
-4 space at the end of this line    
-]=]
